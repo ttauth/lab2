4fc41c66b28d85ebfb4b2a4fe2fa3a735fa39b84
Tyler Auth <76677891+ttauth@users.noreply.github.com>
2023-09-24 20:11:19 -0500

    "Finished"

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -2,6 +2,7 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     public void clickFunction(View view){
         //Log.i("INFO", "Button Pressed");
         EditText myTextField = (EditText) findViewById(R.id.myTextField);
-        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity(myTextField.getText().toString());
+    }
+    public void goToActivity(String s){
+        Intent intent=new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
