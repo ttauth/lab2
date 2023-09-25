6bc94d4bbdbfd5c19715cd39cc1d26a1aa2af5ad
Tyler Auth <76677891+ttauth@users.noreply.github.com>
2023-09-24 19:54:47 -0500

    "Added text field and button, need to do calculator"

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -3,12 +3,22 @@ package com.cs407.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void clickFunction(View view){
+        //Log.i("INFO", "Button Pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
